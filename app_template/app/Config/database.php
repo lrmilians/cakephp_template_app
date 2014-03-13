e8e581bfe1cc0dffb70cda92e3931eee269f1cfc
Lazaro Raisel Milians Alvarez de la Campa <lazaro.milians@numencorp.com>
2014-03-13 16:49:00 -0500

    Subiendo cambios.

---
+++ app_template/app/Config/database.php
@@ -0,0 +1,12 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'cakephp',
+		'password' => '5LS8XdSeWapP2tQp',
+		'database' => 'cakephp',
+	);
+}
